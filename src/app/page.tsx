cc357df357e1c3b6e2e541b35c3ef50b609d4bec
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-06 13:14:34 +0300

    Add HeaderMenu component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import React from 'react';
+import HeaderMenu from '../components/HeaderMenu';
 
 export default function HomePage() {
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1 container mx-auto px-4 py-8">
         {/* כאן יתווספו הקומפוננטות שייווצרו על ידי המחולל */}
-      </main>
+        <HeaderMenu />
+  </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
         <div className="container mx-auto px-4 text-center text-gray-500 text-sm">
